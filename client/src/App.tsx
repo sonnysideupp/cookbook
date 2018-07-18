3710464acee006c564574d0720749732d2401f90
Sonny Huang <shengh@princeton.edu>
2018-07-18 16:33:20 +0100

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { hot } from "react-hot-loader"
 import { Switch, Route } from "react-router-dom"
 import HomePage from "./components/home-page/home-page"
-
+import CreateRecipeForm from "./components/create-recipe/create-recipe"
 import MyProfile from "./components/my-profile/my-profile"
 // import HomePage2 from "./components/home-page2/home-page2"
 import Post from "./components/post_click/post"
@@ -15,8 +15,14 @@ class App extends React.Component {
         <Switch>
           <Route exact={true} path="/" component={HomePage} />
           <Route exact={true} path="/post" component={Post} />
+
           {/* <Route exact={true} path="/HomePage2" component={HomePage2} /> */}
           <Route exact={true} path="/my-profile" component={MyProfile} />
+          <Route
+            exact={true}
+            path="/my-profile/create-recipe"
+            component={CreateRecipeForm}
+          />
         </Switch>
       </div>
     )
