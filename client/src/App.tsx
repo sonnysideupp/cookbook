76d11f14bbcc4e85bf118204f62f69ea14de37d3
Yudai Yaguchi <yyaguchi@wisc.edu>
2018-07-18 16:34:26 +0100

    home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { hot } from "react-hot-loader"
 import { Switch, Route } from "react-router-dom"
 import HomePage from "./components/home-page/home-page"
+import Signup from "./components/signup/signup"
 
 import MyProfile from "./components/my-profile/my-profile"
 // import HomePage2 from "./components/home-page2/home-page2"
@@ -17,6 +18,7 @@ class App extends React.Component {
           <Route exact={true} path="/post" component={Post} />
           {/* <Route exact={true} path="/HomePage2" component={HomePage2} /> */}
           <Route exact={true} path="/my-profile" component={MyProfile} />
+          <Route exact={true} path="/my-profile" component={Signup} />
         </Switch>
       </div>
     )
