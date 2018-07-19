92698c3b13308c2df2beaac3ca802133a05000a3
Sonny Huang <shengh@princeton.edu>
2018-07-19 22:07:50 +0100

    unlike

--- server/src/resolvers/Mutation/unlikerecipe.ts
+++ server/src/resolvers/Mutation/unlikerecipe.ts
@@ -1,16 +1,23 @@
-// import { Context, getUserId, getLikeId } from "../../utils"
-// export default {
-//   unlikerecipe: async (parent, args, ctx: Context, info) => {
-//     const id = getUserId(ctx)
-//     const like = await getLikeId(args.name, ctx, info)
-//     const likeid = like[0].id
-//     return await ctx.db.mutation.deleteLike(
-//       {
-//         where: {
-//           id: likeid
-//         }
-//       },
-//       info
-//     )
-//   }
-// }
+import { Context, getUserId } from "../../utils"
+export default {
+  unlikerecipe: async (parent, args, ctx: Context, info) => {
+    const id = getUserId(ctx)
+
+    const [like] = await ctx.db.query.likes(
+      { where: { recipe: { name: args.name }, author: { id: id } } },
+      info
+    )
+    if (!like) {
+      throw new Error("No Like.")
+    }
+
+    return await ctx.db.mutation.deleteLike(
+      {
+        where: {
+          id: like.id
+        }
+      },
+      info
+    )
+  }
+}
