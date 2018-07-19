9d036d802eacd6225b75ec7c8f990c5702d337e8
Yudai Yaguchi <yyaguchi@wisc.edu>
2018-07-19 12:45:25 +0100

    modified card so that it is connected to backend

--- client/src/components/home-page/home-page.jsx
+++ client/src/components/home-page/home-page.jsx
@@ -1,6 +1,4 @@
 import * as React from "react"
-// import Navigation from "../navigation/navigation"
-// import DropdownFoodtype from "../dropdown-foodtype/dropdown-foodtype"
 import Foodtype from "../foodtype/foodtype"
 import FoodWindow from "../card/card"
 import FoodSlide from "../carousel/carousel"
@@ -26,15 +24,6 @@ class HomePage extends React.Component {
         <Navbar history={this.props.history} />
         <FoodSlide />
         <Foodtype />
-
-        {/* <div>
-          {this.state.selectedFoodType}
-          <select onChange={this.onFoodTypeChange}>
-            <option label="sea food" value="seafood" />
-            <option label="chinese food" value="chinesefood" />
-            <option label="japanese food" value="japanesefood" />
-          </select>
-        </div> */}
 
         <div className="cards">
           <FoodWindow />
